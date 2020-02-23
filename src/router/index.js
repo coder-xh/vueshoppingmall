7842ddf70f29facdd44edf74509068fd7c89edf9
coder-xh <7974571193@qq.com>
2020-02-23 23:09:05 +0800

    完成首页轮播图与推荐部分

---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+const originalPush = VueRouter.prototype.push;
+
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+};
+
+// const Home = () => import()
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "",
+    redirect: "/home"
+  },
+  {
+    path: "/home",
+    component: () => import("views/home/Home")
+  },
+  {
+    path: "/category",
+    component: () => import("views/category/Category")
+  },
+  {
+    path: "/profile",
+    component: () => import("views/profile/Profile")
+  },
+  {
+    path: "/cart",
+    component: () => import("views/cart/Cart")
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: "history"
+});
+
+export default router;
---
+++ src/views/home/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommend="recommend"></recommend-view>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+
+import { getHomeMultidata } from "network/home";
+
+export default {
+  data() {
+    return {
+      banners: [],
+      recommend: []
+    };
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommend = res.data.recommend.list;
+    });
+  },
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  methods: {}
+};
+</script>
+
+<style>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
